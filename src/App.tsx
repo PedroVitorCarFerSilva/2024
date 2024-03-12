e88b526c60bb80b68b25d724edfec2051fcd01c5
Pedro Vitor <93941657+PedroVitorCarFerSilva@users.noreply.github.com>
2024-03-12 17:27:20 -0300

    Botaos adicionados no app

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ const App = () => {
     return (
       <div>
         <h1>Bem vindo ao mundo React</h1>
-        <MeuBotao/>
-        <MeuBotao/>
-        <MeuBotao/>
+        <MeuBotao />
+        <MeuBotao />
+        <MeuBotao />
       </div>
     );
   }
